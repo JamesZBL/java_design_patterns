d3f4441f416c336feadf60344e5610649165590c
James <1146556298@qq.com>
2017-11-09 22:24:33 +0800

    增加创建者模式

--- builder/src/main/java/me/zbl/builder/Application.java
+++ builder/src/main/java/me/zbl/builder/Application.java
@@ -35,9 +35,12 @@ public class Application {
 
   public static void main(String[] args) {
 
-    Person personWang = new Person.Builder().name("小王").age(25).nationality(Nationality.CHINA).skinColor(SkinColor.YELLOW).build();
-    Person personZhang = new Person.Builder().name("小张").age(28).nationality(Nationality.USA).skinColor(SkinColor.WHITE).build();
-    Person personLiu = new Person.Builder().name("老王").age(48).nationality(Nationality.JAPAN).skinColor(SkinColor.YELLOW).build();
+    Person personWang =
+            new Person.Builder().name("小王").age(25).nationality(Nationality.CHINA).skinColor(SkinColor.YELLOW).build();
+    Person personZhang =
+            new Person.Builder().name("小张").age(28).nationality(Nationality.USA).skinColor(SkinColor.WHITE).build();
+    Person personLiu =
+            new Person.Builder().name("老王").age(48).nationality(Nationality.JAPAN).skinColor(SkinColor.YELLOW).build();
 
     LOGGER.info(personWang.toString());
     LOGGER.info(personZhang.toString());

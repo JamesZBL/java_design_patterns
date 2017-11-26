a02190521c36e3b17ed0269c1d1ff30daea96d13
James <1146556298@qq.com>
2017-11-26 23:15:24 +0800

    完成 迭代器模式 Sample

--- iterator/src/main/java/BookShelf.java
+++ iterator/src/main/java/BookShelf.java
@@ -22,10 +22,34 @@
  * SOFTWARE.
  */
 
-/**
- * 元素类型
- */
-public enum ItemType {
+import java.util.ArrayList;
+import java.util.List;
 
-  IT, FICTION, CARTOON
+/**
+ * 书架
+ */
+public class BookShelf {
+
+  private List<Item> items;
+
+  public BookShelf() {
+    items = new ArrayList<>();
+    items.add(new Item(ItemType.FICTION, "西游记"));
+    items.add(new Item(ItemType.FICTION, "水浒传"));
+    items.add(new Item(ItemType.FICTION, "三国演义"));
+    items.add(new Item(ItemType.FICTION, "红楼梦"));
+    items.add(new Item(ItemType.CARTOON, "阿衰"));
+    items.add(new Item(ItemType.CARTOON, "七龙珠"));
+    items.add(new Item(ItemType.CARTOON, "火影忍者"));
+    items.add(new Item(ItemType.IT, "设计模式-可复用面向对象软件的基础"));
+    items.add(new Item(ItemType.IT, "重构-改善既有代码的设计"));
+    items.add(new Item(ItemType.IT, "Effective Java"));
+    items.add(new Item(ItemType.IT, "Java编程思想"));
+  }
+
+  public List<Item> getItemList() {
+    List<Item> list = new ArrayList<>();
+    list.addAll(items);
+    return list;
+  }
 }
